2833be4e10fbe3ba29dd8a2462b56e032a573eb3
TimothyTruong <timothyttruong29@gmail.com>
2024-03-16 07:56:00 -0400

    UserID in user-data + Post Fetch Functions

--- public/mediaFeed/mediaFeed.js
+++ public/mediaFeed/mediaFeed.js
@@ -1,3 +1,15 @@
+/*
+{
+  profilePicture: 'default-image-url',
+  CO2Absorbed: 0,
+  TreesPlanted: 0,
+  username: 'normanbui',  
+  userId: "m6rADXvLnFVSSASkSdF9Fq8TshT2",
+  joinedDate: Timestamp { _seconds: 1710474379, _nanoseconds: 542000000 }
+}
+
+*/
+
 function fetchUserData() {
     const idToken = localStorage.getItem('idToken'); // Retrieve the token
     if (!idToken) {
@@ -24,6 +36,8 @@ function fetchUserData() {
     });
 }
 
+
+
 function getPosts(){
     fetch('/api/posts')
     .then(response => {
@@ -33,16 +47,91 @@ function getPosts(){
         return response.json();
     })
     .then(posts => {
-        console.log("Posts fetched successfully:", posts);
-    })
+       // console.log("Posts fetched successfully:", posts);
+        return posts;
+
+    });
 }
+
+function returnArray(){
+
+}
+
+// function getPostById(paramPostId){
+//     //fetch post by id in the URL
+//     fetch('api/posts/' + paramPostId, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//     })
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Failed to fetch post');
+//         }
+//         return response.json();
+//     });
+// }
 
 function addComment(){
     
 }
 
-function toggleLike(){
-
+async function toggleLike(postId){
+    posts = await getPosts();
+    console.log(posts)
+    // let post = posts.find(post => post.postId === postId);
+    // console.log(post);
+    // if(post.likes.includes(userId)){
+    //     //unlike
+    //     fetch('/api/posts/unlike', {
+    //         method: 'PATCH',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             postId: postId,
+    //             userId: userId
+    //         })
+    //     })
+    //     .then(response => {
+    //         if (!response.ok) {
+    //             throw new Error('Failed to unlike post');
+    //         }
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         console.log("Post unliked successfully:", data);
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
+    // }
+    // else {
+    //     //like
+    //     fetch('/api/posts/like', {
+    //         method: 'PATCH',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             postId: postId,
+    //             userId: userId
+    //         })
+    //     })
+    //     .then(response => {
+    //         if (!response.ok) {
+    //             throw new Error('Failed to like post');
+    //         }
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         console.log("Post liked successfully:", data);
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
+    // }
 }
 
 function editPost(){
